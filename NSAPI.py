cdbcf7581c2427233f89c1eaef1cc990ed431019
rootabeta <rootabeta@protonmail.com>
2023-09-02 02:31:33 -0400

    Enabled basic IFF

--- NSAPI.py
+++ NSAPI.py
@@ -32,21 +32,52 @@ def regionAPI(url, user_agent):
     parsed_response: benedict = parsed_response["region"]  # type: ignore
     return parsed_response
 
+def nationAPI(url, user_agent):
+    r = requests.get(url, headers={"User-Agent":user_agent})
+
+    head = r.headers
+
+    if "X-Pin" in head:
+        self.pin = head["X-Pin"]
+
+    if waiting_time := head.get("Retry-After"):
+        print(f"\r[!] Rate limited. Waiting {waiting_time} seconds.")
+        time.sleep(int(waiting_time))
+    # slow down requests so we dont hit the rate limit in the first place
+    requests_left = int(head["RateLimit-Remaining"])
+
+    # If we have <10 requests left in this window, sloooow down
+    if requests_left < 10:
+        seconds_until_reset = int(head["RateLimit-Reset"])
+        delay = seconds_until_reset / requests_left
+
+        # A 700ms delay is invoked by the parent function
+        # If that is enough, don't sleep more. Otherwise, sleep that long
+        if delay > 700:
+            time.sleep(699 - delay)
+
+    parsed_response = benedict.from_xml(r.text, keyattr_dynamic=True)
+    parsed_response.standardize()
+    parsed_response: benedict = parsed_response["nation"]  # type: ignore
+    return parsed_response
+
 # Get a given shard for a given region
 def getRegionShard(region, shard, user):
     # nationstates.net/cgi-bin/api.cgi?region=greater_sahara&q=nations
     if not user:
         raise RuntimeError("We need a user to identify you")
 
-    userAgent = f"Brimstone/0.3 (API component); Developed by nation=Volstrostia; in use by nation={user}"
+    userAgent = f"Brimstone/0.4 (API component); Developed by nation=Volstrostia; in use by nation={user}"
     URL = f"https://nationstates.net/cgi-bin/api.cgi?region={region}&q={shard}"
     return regionAPI(URL, userAgent)
 
 # Get the region a nation resides in
 def getRegion(nation, user):
-    userAgent = f"Brimstone/0.3 (API component); Developed by nation=Volstrostia; in use by nation={user}"
+    userAgent = f"Brimstone/0.4 (API component); Developed by nation=Volstrostia; in use by nation={user}"
     URL = f"https://nationstates.net/cgi-bin/api.cgi?nation={nation}&q=region"
-    region = regionAPI(URL, userAgent)
+    residingregion = nationAPI(URL, userAgent)
+    if residingregion and "region" in residingregion:
+        return residingregion["region"]
 
 # Get list of all nations
 def getNations(region, user):
@@ -75,4 +106,28 @@ def getWANations(region, user):
             return []
     else:
         return []
+
+def getROs(region, user):
+    ROs = getRegionShard(region, "officers+delegate", user)
+    officers = []
+
+    if ROs and "delegate" in ROs:
+        if str(ROs.delegate) != "0":
+            officers.append(ROs.delegate)
+
+    if ROs and "officers" in ROs:
+        raw = ROs.officers.officer
+        ROList = []
+        if type(raw) != list:
+            raw = [raw] # If there's only one RO, NS will not return it as an array... for some reason
+
+        for office in raw:
+            if office.nation:
+                officers.append(office.nation)
+
+    if officers:
+        return officers
+    else:
+        return []
+
 
--- brimstone.py
+++ brimstone.py
@@ -20,7 +20,7 @@ def fetch_nations(user, region, WA_only):
     return nationlist
 
 # Constantly poll the API for nations
-def track_inbounds(user, region, inbound, WA_only):
+def track_inbounds(user, region, inbound, WA_only, ROs):
     print("Radar online. Keeping our eye on the sky.")
     oldNations = []
     newNations = []
@@ -44,8 +44,12 @@ def track_inbounds(user, region, inbound, WA_only):
             # This nation wasn't here the last time we checked! Add it to the list of inbounds
             for nation in newNations:
                 if canonicalize(nation) not in oldNations and canonicalize(nation) not in inbound:
-                    print(f"\r[!] Radar detected inbound bogey: {canonicalize(nation)}")
-                    inbound.append(canonicalize(nation))
+                    if canonicalize(nation) not in ROs:
+                        # Not an RO, and just appeared - blow em to smithereens
+                        print(f"\r[!] Radar detected inbound bogey: {canonicalize(nation)}")
+                        inbound.append(canonicalize(nation))
+                    else:
+                        print(f"\r[*] Radar detected inbound friendly - avoiding buddyspike: {canonicalize(nation)}")
             
             # Now, set the state to the new one, to detect next second
             oldNations = newNations
@@ -59,16 +63,31 @@ def main():
         print(f.read())
 
     user = input("Your main nation: ")
-    session = NSSession("Brimstone","0.3","Volstrostia",user)
 
-    region = canonicalize(input("Region: ")) # TODO: Track from nation
+    print("Initializing SAM site, please wait...")
+    session = NSSession("Brimstone","0.4","Volstrostia",user)
+    print("SAM site ready to continue setup")
+#    region = canonicalize(input("Region: ")) # TODO: Track from nation
 
     nation = canonicalize(input("RO Nation: "))
     password = getpass("Password: ")
+    region = canonicalize(NSAPI.getRegion(nation,user))
+    print(f"Preparing to eliminate incursions into the airspace of: {region.upper()}")
 
     WA_only = False
     WA_only = True if str(input("Only track WA? (y/N) ")).lower().startswith("y") else False
     print("Tracking WA movement" if WA_only else "Tracking movement")
+
+    spare_ROs = True
+    spare_ROs = False if str(input("Exempt ROs from targetting? (Y/n) ")).lower().startswith("n") else True
+
+    ROs = []
+    if spare_ROs:
+        print("Sparing ROs from the wrath of Brimstone")
+        print("Initializing IFF system")
+        for RO in NSAPI.getROs(region, user):
+            print(f"Identified RO: {RO}")
+            ROs.append(canonicalize(RO)) # Just in case
 
     print("SAM site initialized. Press SPACE to arm missiles.")
     if session.login(nation, password):
@@ -77,7 +96,7 @@ def main():
         inbound = manager.list()
 
         # Open a thread to run track_inbounds (poll API regularly for nations)
-        radar = Process(target=track_inbounds, args=(user, region, inbound, WA_only))
+        radar = Process(target=track_inbounds, args=(user, region, inbound, WA_only, ROs))
         radar.start()
 
         print("\rBird affirm. Prepared to engage.")
